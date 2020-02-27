222d2bfff1060aed39893cf96f4eb52d5c7623a7
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-02-26 20:42:52 -0500

    Added the PlayerCard Component and added state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import PlayerCard from './PlayerCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Game extends Component {
+
+  constructor() {
+    super();
+    this.signs = ['rock', 'scissors', 'paper']
+    this.state = {
+      playerOne: 'rock',
+      playerTwo: 'scissors'
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <PlayerCard sign={this.state.playerOne} />
+          <PlayerCard sign={this.state.playerTwo} />
+        </div>
+        <div className="winner">Here is the winner</div>
+        <button type="button">Play Game</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Game;
